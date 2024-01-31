dd3a3aa6cdccec15d2e871aa1b7143b6c01d46a9
Nehoa21 <nehoaakina21@yahoo.com>
2024-01-30 22:51:31 -0700

    added logic to create/add to database and install PWA

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const contactDb = await openDB('jate', 1);
+  const tAction = contactDb.transaction('jate', 'readwrite');
+  const store = tAction.objectStore('jate');
+  const req = store.put({ jate: content });
+  const res = await req;
+  return res;
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const contactDb = await openDB('jate', 1);
+  const tAction = contactDb.transaction('jate', 'readonly');
+  const store = tAction.objectStore('jate');
+  const req = store.getAll();
+  const res = await req;
+  return res;
+};
 
 initdb();
